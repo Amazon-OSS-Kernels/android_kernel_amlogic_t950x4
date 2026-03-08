b267950a6b2b38b8e188a75d7ebe69e590dc4f71
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 11:11:31 -0500

    Base release import: TCL_Class_S3_1080p_LED_Smart_TV_with_Fire_TV-7.6.5.8-20230831

---
+++ build_uboot_config.sh
@@ -0,0 +1,22 @@
+################################################################################
+# 
+#  build_kernel_config.sh
+#
+#  Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+################################################################################
+
+UBOOT_SUBPATH="bootable/bootloader/uboot-amlogic/t950x4/"
+UBOOT_DEFCONFIG_NAME="almond"
+export UFBL_PLAT_PROJ=aml_t950x4
+
+# Expected image files are seperated with ":"
+UBOOT_IMAGES="build/u-boot.bin"
+
+################################################################################
+# NOTE: You must fill in the following with the path to a copy of an
+#       gcc-linaro-aarch64-none-elf-4.8-2013.11_linux (aarch64-none-elf compiler) and
+#       riscv-none-gcc/7.2.0-4-20180606-1631
+################################################################################
+CROSS_COMPILER_PATH=""
+RISCV_COMPILER_PATH=""
